3edc3f812a9e57896dea804c68f8d8d26448bc19
qzjios1@gmail.com <qzj123456>
2020-05-08 14:34:51 +0800

    update mysql

--- PythonCar/venv/src/访问数据库/使用MySQL.py
+++ PythonCar/venv/src/访问数据库/使用MySQL.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
 
-
 '''
 MySQL是Web世界中使用最广泛的数据库服务器。SQLite的特点是轻量级、可嵌入，但不能承受高并发访问，适合桌面和移动应用。而MySQL是为服务器端设计的数据库，能承受高并发访问，同时占用的内存也远远大于SQLite。
 
@@ -16,3 +15,42 @@ MySQL是Web世界中使用最广泛的数据库服务器。SQLite的特点是轻
 在Mac或Linux上，需要编辑MySQL的配置文件，把数据库默认的编码全部改为UTF-8。MySQL的配置文件默认存放在/etc/my.cnf或者/etc/mysql/my.cnf：
 
 '''
+
+
+# 导入MySQL驱动:
+import mysql.connector
+import pymysql
+
+# 注意把password设为你的root口令:
+#conn = mysql.connector.connect(user='root',password='password',database='test')
+db_host = "localhost"
+db_username = "root"
+db_password = "password"
+db_name = "test"
+conn = pymysql.connect(
+    host=db_host,
+    user=db_username,
+    passwd=db_password,
+    database=db_name,
+)
+
+
+cursor = conn.cursor()
+
+# 创建user表:
+cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+# 插入一行记录，注意MySQL的占位符是%s:
+cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'carey'])
+print(cursor.rowcount)
+
+# 提交事务:
+conn.commit()
+conn.close()
+
+# 运行查询:
+cursor = conn.cursor()
+cursor.execute('select * from user where id = %s', ('1',))
+values = cursor.fetchall()
+print(values)
+cursor.close()
+conn.close()
